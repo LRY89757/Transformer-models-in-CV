7c40a166a336ea51fb1d02dd5e076fc4b2b15797
LRY89757 <lry89757@gmail.com>
2022-02-25 22:46:57 +0800

    :beers: && :construction: Writing codes drunkenly

--- models/Swin_Transformer/swin_transformer.py
+++ models/Swin_Transformer/swin_transformer.py
@@ -1,4 +1,6 @@
 import sys,os
+
+from cv2 import norm
 sys.path.append(os.getcwd())
 from turtle import forward
 import torch
@@ -8,6 +10,8 @@ import torch.nn.functional as F
 import math
 
 from utils.weight_init import trunc_normal_
+from utils.dot_product import scaled_dot_product
+from utils.tools import DropPath
 
 class MLP(nn.Module):
     def __init__(self, in_dim, hidden_dim, out_dim, layer_num, acti_layer=nn.GELU, dropout=0.0):
@@ -95,7 +99,7 @@ class WindowAttention(nn.Module):
 
         head_dim = dim // num_heads
         self.scale = qk_scale or math.sqrt(head_dim)
-
+        self.num_heads = num_heads
         self.window_size = window_size
 
         # position table
@@ -130,6 +134,107 @@ class WindowAttention(nn.Module):
             mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
         """
         
+        B_, N, C = x.shape
+        qkv = self.qkv(x)
+        q, k, v = qkv.reshape(B_, N,  self.num_heads, C // self.num_heads, 3).permute(0, 2, 1, 3, 4).chunck(3, dim=-1)
+
+        relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
+            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
+        relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
+        bias = relative_position_bias.unsqueeze(0)
+
+        attn = scaled_dot_product(q, k, v, bias=bias)
+        attn = self.attn_drop(attn)
+
+        attn = attn.permute(0, 2, 1, 3)
+        attn = attn.reshape(B_, N, -1)
+
+        out = self.proj(attn)
+        out = self.proj_drop(out)
+
+        return out
+
+    def extra_repr(self) -> str:
+        return f'dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}'
+
+    def flops(self, N):
+        # calculate flops for 1 window with token length of N
+        flops = 0
+        # qkv = self.qkv(x)
+        flops += N * self.dim * 3 * self.dim
+        # attn = (q @ k.transpose(-2, -1))
+        flops += self.num_heads * N * (self.dim // self.num_heads) * N
+        #  x = (attn @ v)
+        flops += self.num_heads * N * N * (self.dim // self.num_heads)
+        # x = self.proj(x)
+        
+        
+        flops += N * self.dim * self.dim
+        return flops
+
+
+class SwinTransformerBlock(nn.Module):
+    r""" Swin Transformer Block.
+
+    Args:
+        dim (int): Number of input channels.
+        input_resolution (tuple[int]): Input resulotion.
+        num_heads (int): Number of attention heads.
+        window_size (int): Window size.
+        shift_size (int): Shift size for SW-MSA.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop (float, optional): Dropout rate. Default: 0.0
+        attn_drop (float, optional): Attention dropout rate. Default: 0.0
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+    """
+    def __init__(self, dim, input_resolution, num_heads, window_size, shift_size,
+         mlp_ratio, qkv_bias, qk_scale, drop=0.0, attn_drop=0.0, 
+         drop_path=0.0, act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.num_heads = num_heads
+        self.shift_size = shift_size
+        self.window_size = window_size
+
+        if self.window_size >= min(self.input_resolution):
+            self.window_size = min(self.input_resolution)
+            self.shift_size = 0
+        assert 0<=self.shift_size<=self.window_size, "shift size must be smaller than the window size."
+
+        self.attn = WindowAttention(
+            dim, window_size, num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=drop
+        )
+        
+        self.norm1 = norm_layer(dim)
+        self.norm2 = norm_layer(dim)
+        hidden_mlp = dim * mlp_ratio
+        self.mlp = MLP(dim, hidden_mlp, out_dim=dim, layer_num=3, acti_layer=act_layer, dropout=drop)
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
 
 
 
