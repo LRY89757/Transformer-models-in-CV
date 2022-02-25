92abf22639f804cbe48fb187b3aa4afb7f5f6d50
LRY89757 <lry89757@gmail.com>
2022-02-25 22:45:42 +0800

    :boom: Add the bias and mask for scaled dot product, this is for swin-T

--- utils/dot_product.py
+++ utils/dot_product.py
@@ -2,15 +2,22 @@ import torch
 import torch.nn.functional as F
 import math
 
-def scaled_dot_product(Q, K, V, return_att=False):
+def scaled_dot_product(Q, K, V, return_att=False, bias=None, mask=None):
     '''
     Input: Q,K,V shape may be [Batch, Heads, T, Dk], 
             Dk is the dim of the token, T is the length of the sequence.
+           bias: default None, if not None, it will be the position bias added to the qk^T
+           mask: default None, if not None, it will be the mask code for swin transformer.
     output: [B, H, T, Dk], [B, H, T, T]
     '''
     Dk = Q.size()[-1]
     K_T = K.transpose(-1, -2)
-    att_weight = F.softmax(torch.matmul(Q, K_T) / math.sqrt(Dk), dim=-1)
+    if bias is None:   
+        att_weight = F.softmax(torch.matmul(Q, K_T) / math.sqrt(Dk), dim=-1)
+    elif mask is None:
+        att_weight = F.softmax(torch.matmul(Q, K_T) / math.sqrt(Dk) + bias, dim=-1)
+    else:
+        att_weight = F.softmax(torch.matmul(Q, K_T) / math.sqrt(Dk) + bias + mask, dim=-1)
     mean_values = torch.matmul(att_weight, V)
 
     if return_att:
