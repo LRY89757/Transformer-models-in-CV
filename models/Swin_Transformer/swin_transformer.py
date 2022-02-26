d48925f52ab00946ee6c264eca8f8c1c3cdf3895
LRY89757 <lry89757@gmail.com>
2022-02-26 17:13:28 +0800

    :beers: Finish the basic component of Transformer

--- models/Swin_Transformer/swin_transformer.py
+++ models/Swin_Transformer/swin_transformer.py
@@ -1,7 +1,10 @@
 from curses import window
+from re import X
 import sys,os
 
 from cv2 import norm
+from matplotlib import use
+from matplotlib.cbook import normalize_kwargs
 sys.path.append(os.getcwd())
 from turtle import forward
 import torch
@@ -319,10 +322,204 @@ class PatchMerging(nn.Module):
         dim (int): Number of input channels.
         norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
     """
-    def __init__(self, input_resolution, dim, normlayer=nn.LayerNorm):
+    def __init__(self, input_resolution, dim, norm_layer=nn.LayerNorm):
         super().__init__()
-        self.normlayer = normlayer(dim)
         self.input_resolution = input_resolution
+        self.dim = dim
+        self.reduction = nn.Linear(4 * dim, 2 * dim, bias=False)
+        self.norm = norm_layer(4 * dim)
+
+
+    
+    def forward(self, x):
+        """
+        x: B, H*W, C
+        """
+        B, L, C = x.shape
+        H, W = self.input_resolution
+        assert L == H * W, "the shape of H, W and L doesn't match" 
+        assert self.dim == C, "the dim and the Channels don't match"
+
+        x = x.view(B, H, W, C)
+        X0 = x[:, 0::2, 0::2, :]        
+        X1 = x[:, 1::2, 0::2, :]
+        X2 = x[:, 1::2, 1::2, :]
+        X3 = x[:, 0::2, 1::2, :]
+        x = torch.cat([X0, X1, X2, X3], dim=-1)
+
+        x = x.view(B, -1, 4*C)
+        x = self.norm(x)
+
+        x = self.reduction(x)
+
+        return x
+
+    def extra_repr(self) -> str:
+        return f"input_resolution={self.input_resolution}, dim={self.dim}"
+
+    def flops(self):
+        H, W = self.input_resolution
+        flops = H * W * self.dim
+        flops += (H // 2) * (W // 2) * 4 * self.dim * 2 * self.dim
+        return flops
+
+
+
+ 
+class BasicLayer(nn.Module):
+    """ A basic Swin Transformer layer for one stage.
+
+    Args:
+        dim (int): Number of input channels.
+        input_resolution (tuple[int]): Input resolution.
+        depth (int): Number of blocks.
+        num_heads (int): Number of attention heads.
+        window_size (int): Local window size.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop (float, optional): Dropout rate. Default: 0.0
+        attn_drop (float, optional): Attention dropout rate. Default: 0.0
+        drop_path (float | tuple[float], optional): Stochastic depth rate. Default: 0.0
+        norm_layer (nn.Module, optional): Normalization layer. Default: nn.LayerNorm
+        downsample (nn.Module | None, optional): Downsample layer at the end of the layer. Default: None
+        use_checkpoint (bool): Whether to use checkpointing to save memory. Default: False.
+    """
+    def __init__(self, dim, input_resolution, depth, num_heads, window_size, 
+            mlp_ratio, qkv_bias, qk_scale, drop=0.0, attn_drop=0.0, drop_path=0.0,
+            norm_layer=nn.LayerNorm, downsample=None, use_checkpoint=False):
+        super().__init__()
+
+        assert depth % 2 == 0, "the depth of the layer must be zero."
+
+        if downsample is not None:
+            self.downsample = downsample(input_resolution, dim, norm_layer=norm_layer)
+        else:
+            self.downsample = None
+        
+        self.Swin_T = nn.ModuleList([SwinTransformerBlock(dim, input_resolution, num_heads, window_size, 
+                                        0 if i % 2 == 0 else window_size //2, mlp_ratio, qkv_bias, qk_scale, 
+                                        drop, attn_drop=attn_drop, drop_path=drop_path) 
+                                                for i in depth])
+        
+        self.use_checkpoint = use_checkpoint
+
+    def forward(self, x):
+
+        for block in self.Swin_T:
+            if self.use_checkpoint:
+                x = checkpoint.checkpoint(block, x)
+            else:
+                x = block(x)
+
+        if self.downsample:
+            x = self.downsample(x)
+
+        return x
+
+    def extra_repr(self) -> str:
+        return f"dim={self.dim}, input_resolution={self.input_resolution}, depth={self.depth}"
+
+    def flops(self):
+        flops = 0
+        for blk in self.blocks:
+            flops += blk.flops()
+        if self.downsample is not None:
+            flops += self.downsample.flops()
+        return flops
+
+
+ 
+class PatchEmbed(nn.Module):
+    r""" Image to Patch Embedding
+
+    Args:
+        img_size (int): Image size.  Default: 224.
+        patch_size (int): Patch token size. Default: 4.
+        in_chans (int): Number of input image channels. Default: 3.
+        embed_dim (int): Number of linear projection output channels. Default: 96.
+        norm_layer (nn.Module, optional): Normalization layer. Default: None
+    """
+
+    def __init__(self, img_size, patch_size, in_chans, embed_dim, norm_layer):
+        super().__init__()
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, patch_size, patch_size)
+
+        self.norm = norm_layer
+
+    def forward(self, x):
+        '''
+        x's shape: [B, C, H, W]
+        '''
+
+        x = self.proj(x).flatten(2).transpose(1, 2)
+
+        if self.norm:
+            x = self.norm(x)
+        
+        return x
+
+    def flops(self):
+        Ho, Wo = self.patches_resolution
+        flops = Ho * Wo * self.embed_dim * self.in_chans * (self.patch_size[0] * self.patch_size[1])
+        if self.norm is not None:
+            flops += Ho * Wo * self.embed_dim
+        return flops
+
+ 
+class SwinTransformer(nn.Module):
+    r""" Swin Transformer
+        A PyTorch impl of : `Swin Transformer: Hierarchical Vision Transformer using Shifted Windows`  -
+          https://arxiv.org/pdf/2103.14030
+
+    Args:
+        img_size (int | tuple(int)): Input image size. Default 224
+        patch_size (int | tuple(int)): Patch size. Default: 4
+        in_chans (int): Number of input image channels. Default: 3
+        num_classes (int): Number of classes for classification head. Default: 1000
+        embed_dim (int): Patch embedding dimension. Default: 96
+        depths (tuple(int)): Depth of each Swin Transformer layer.
+        num_heads (tuple(int)): Number of attention heads in different layers.
+        window_size (int): Window size. Default: 7
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim. Default: 4
+        qkv_bias (bool): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float): Override default qk scale of head_dim ** -0.5 if set. Default: None
+        drop_rate (float): Dropout rate. Default: 0
+        attn_drop_rate (float): Attention dropout rate. Default: 0
+        drop_path_rate (float): Stochastic depth rate. Default: 0.1
+        norm_layer (nn.Module): Normalization layer. Default: nn.LayerNorm.
+        ape (bool): If True, add absolute position embedding to the patch embedding. Default: False
+        patch_norm (bool): If True, add normalization after patch embedding. Default: True
+        use_checkpoint (bool): Whether to use checkpointing to save memory. Default: False
+    """
+    def __init__(self, img_size=224, patch_size=4, in_chans=3, num_classes=1000, 
+                embed_dim=96, depths=[2, 2, 6, 2], nums_head=[3, 6, 12, 24], 
+                window_size=7, mlp_ratio=4, qkv_bias=True, qk_scale=None, 
+                drop_rate=0, attn_drop_rate=0, drop_path_rate=0.1, norm_layer=nn.LayerNorm, 
+                ape=False, patch_norm=True, use_checkpoint=False):
+        super().__init__()
+        
+
+
+
+
+
+       
+
+
+
+        
+
+
+
+
+
+
+
+       
+
+ 
 
 
 
